a768105ab3c666d97ac0960c9f325a52536710b3
Kyryl Cherevko <cherevko022@gmail.com>
2024-11-06 14:44:22 -0500

    feat: add cookie-parser middleware and configure CORS settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
