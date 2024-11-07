860a96235358b3973d11e5d28efc01680c8b0750
Kyryl Cherevko <cherevko022@gmail.com>
2024-11-06 21:43:38 -0500

    feat: implement time block module with controller, service, and DTO for time management

---
+++ src/time-block/time-block.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { TimeBlockDto } from './dto/time-block.dto';
+
+@Injectable()
+export class TimeBlockService {
+  constructor(private prisma: PrismaService) {}
+
+  async getById(timeBlockId: string) {
+    return this.prisma.timeBlock.findUnique({ where: { id: timeBlockId } });
+  }
+
+  async getAll(userId: string) {
+    return this.prisma.timeBlock.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'asc' },
+    });
+  }
+
+  async create(dto: TimeBlockDto, userId: string) {
+    return this.prisma.timeBlock.create({
+      data: {
+        ...dto,
+        user: { connect: { id: userId } },
+      },
+    });
+  }
+
+  async update(
+    dto: Partial<TimeBlockDto>,
+    timeBlockId: string,
+    userId: string,
+  ) {
+    const existingTimeBlock = await this.getById(timeBlockId);
+
+    if (!existingTimeBlock)
+      throw new NotFoundException("Time block doesn't exist");
+
+    return this.prisma.timeBlock.update({
+      where: {
+        id: timeBlockId,
+        userId,
+      },
+      data: dto,
+    });
+  }
+
+  async delete(timeBlockId: string, userId: string) {
+    const existingTimeBlock = await this.getById(timeBlockId);
+
+    if (!existingTimeBlock)
+      throw new NotFoundException("Time block doesn't exist");
+
+    return this.prisma.timeBlock.delete({
+      where: {
+        id: timeBlockId,
+        userId,
+      },
+    });
+  }
+
+  async updateOrder(ids: string[]) {
+    return this.prisma.$transaction(
+      ids.map((id, order) =>
+        this.prisma.timeBlock.update({
+          where: { id },
+          data: { order },
+        }),
+      ),
+    );
+  }
+}
